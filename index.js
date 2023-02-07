55f49dc29fcc86a1e66c481f93a2269d905ad8b2
Aaron Randall <aaronasherrandall@gmail.com>
2023-02-07 16:48:16 +0900

    Interface Update

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const { Configuration, OpenAIApi } = require("openai");
+const express = require('express')
 const configuration = new Configuration({
     organization: "org-xmTUEh3MGGzCb64tIioDeMdI",
-    apiKey: "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",
+    apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 // const response = await openai.listEngines();
 
-async function callApi(){
+
+//create a simple express api that calls the function above
+
+const app = express()
+const port = 3080
+
+app.post('/', async (req, res) => {
     const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: "Say this is a test",
@@ -14,6 +21,12 @@ async function callApi(){
         temperature: 0,
       });
       console.log(response.data.choices[0].text)
-}
+      res.json({
+        data: response.data
+      })
+});
 
-callApi()
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+}); 
+
